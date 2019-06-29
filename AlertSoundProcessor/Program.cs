c0a0cde95aaa9dfde08e774e8e19d5f00928835a
Tobias H. // Tobnac <tobiashanitsch@yahoo.de>
2019-06-29 16:52:48 +0200

    extracted volume, trim, ... data into func+objClass

--- AlertSoundProcessor/Program.cs
+++ AlertSoundProcessor/Program.cs
@@ -9,9 +9,9 @@ namespace AlertSoundProcessor
     {
         public static void Main(string[] args)
         {
-            var path = @"C:\Users\Tobnac\Zeugs\my girls & B & tumblr\todo sort mfg media\hey adora.mp3";
+//            var path = @"C:\Users\Tobnac\Zeugs\my girls & B & tumblr\todo sort mfg media\hey adora.mp3";
 //            var path = @"C:\Users\Tobnac\Documents\My Games\Path of Exile\Zizaran_1maybevaluable.mp3";
-//            var path = @"C:\Users\Tobnac\Documents\My Games\Path of Exile\Zizaran_2currency.mp3";
+            var path = @"C:\Users\Tobnac\Documents\My Games\Path of Exile\Zizaran_2currency.mp3";
 //            var path = @"C:\Users\Tobnac\Documents\My Games\Path of Exile\BexBloopers_2currency_OG.mp3";
 //            var path = @"C:\Users\Tobnac\Documents\My Games\Path of Exile\Mathil_6veryvaluable.mp3";
 //            var path = @"C:\Users\Tobnac\Documents\My Games\Path of Exile\testSong.mp3";
--- AlertSoundProcessor/SoundProcessor.cs
+++ AlertSoundProcessor/SoundProcessor.cs
@@ -7,55 +7,13 @@ namespace AlertSoundProcessor
     {
         public static void NormalizeVolume(string inPath)
         {
-            float max = 0;
-            var iterationLength = 0;
-            var startIteration = 0;
-            var endIteration = 0;
-            const double tolerance = 0.009;
-
             using (var reader = new AudioFileReader(inPath))
-            {   
-                // find the max peak
-                float[] buffer = new float[reader.WaveFormat.SampleRate];
-                int read;
-                do
-                {
-                    read = reader.Read(buffer, 0, buffer.Length);
-                    for (int n = 0; n < read; n++)
-                    {
-                        iterationLength++;
-                        var abs = Math.Abs(buffer[n]);
-
-                        if (abs > tolerance && startIteration == 0)
-                        {
-                            startIteration = iterationLength;
-                        }
-
-                        if (abs > tolerance)
-                        {
-                            endIteration = iterationLength;
-                        }
-
-                        if (abs > max)
-                        {
-                            max = abs;
-                        }
-                    }
-                } while (read > 0);
-                
-                Console.WriteLine($"Max sample value: {max}");
-
-                if (max == 0 || max > 1.0f) throw new InvalidOperationException("File cannot be normalized");
+            {
+                var data = GetKeyValues(reader);
 
                 // rewind and amplify
                 reader.Position = 0;
-                reader.Volume = 1.0f / max;
-
-                double startPercent = (double) startIteration / iterationLength;
-                double endPercent = (double) endIteration / iterationLength;
-                var startTime = TimeSpan.FromMilliseconds(reader.TotalTime.TotalMilliseconds * startPercent); 
-                var endTime = TimeSpan.FromMilliseconds(reader.TotalTime.TotalMilliseconds * endPercent);
-                endTime = reader.TotalTime - endTime;
+                reader.Volume = 1.0f / data.MaxVolume;
 
                 // write out to a new WAV file
                 var path = @"C:\Users\Tobnac\Desktop\output.wav";
@@ -63,12 +21,68 @@ namespace AlertSoundProcessor
                 var pathc = @"C:\Users\Tobnac\Desktop\outputResTwo.mp3";
                 WaveFileWriter.CreateWaveFile16(path, reader);
                 
-                AudioTrimmer.TrimWavFile_Amount(path, pathb, startTime, endTime);
+                AudioTrimmer.TrimWavFile_Amount(path, pathb, data.StartCut, data.EndCut);
 
 //                ConvertToMp3V3(pathb, pathc);
 //                ConvertToMp3(pathb, pathc);
 //                ReWriteAsMp3(pathb, pathc);
             }
+        }
+
+        private static AudioKeyValues GetKeyValues(AudioFileReader reader)
+        {
+            var res = new AudioKeyValues();
+            const double tolerance = 0.009;
+            var startIteration = 0;
+            var endIteration = 0;
+                
+            // find the max peak
+            float[] buffer = new float[reader.WaveFormat.SampleRate];
+            int read;
+            do
+            {
+                read = reader.Read(buffer, 0, buffer.Length);
+                for (int n = 0; n < read; n++)
+                {
+                    res.IterationCount++;
+                    var volume = Math.Abs(buffer[n]);
+
+                    if (volume > tolerance && startIteration == 0)
+                    {
+                        startIteration = res.IterationCount;
+                    }
+
+                    if (volume > tolerance)
+                    {
+                        endIteration = res.IterationCount;
+                    }
+
+                    if (volume > res.MaxVolume)
+                    {
+                        res.MaxVolume = volume;
+                    }
+                }
+            } while (read > 0);
+
+            Console.WriteLine($"Max sample value: {res.MaxVolume}");
+
+            if (res.MaxVolume == 0 || res.MaxVolume > 1.0f) throw new InvalidOperationException("File cannot be normalized");
+            
+            double startPercent = (double) startIteration / res.IterationCount;
+            double endPercent = (double) endIteration / res.IterationCount;
+            res.StartCut = TimeSpan.FromMilliseconds(reader.TotalTime.TotalMilliseconds * startPercent); 
+            res.EndCut = TimeSpan.FromMilliseconds(reader.TotalTime.TotalMilliseconds * endPercent);
+            res.EndCut = reader.TotalTime - res.EndCut;
+            
+            return res;
+        }
+
+        public class AudioKeyValues
+        {
+            public int IterationCount { get; set; }
+            public float MaxVolume { get; set; }
+            public TimeSpan StartCut { get; set; }
+            public TimeSpan EndCut { get; set; }
         }
     }
 }
